4926c9e78e7d527786090efefda89b9ad4c35e68
Sahinul Haque <sahinul.haque@itpeoplecorp.com>
2022-03-25 12:29:46 +0530

    for create auction house and file type validation
    Signed-off-by: Sahinul Haque <sahinul.haque@itpeoplecorp.com>

--- auction-ui/src/components/NewArtwork.js
+++ auction-ui/src/components/NewArtwork.js
@@ -62,7 +62,11 @@ class NewArtwork extends Component {
     let file = event.target.files[0];
     //validating
     let fileSize = parseFloat(file["size"] || 0) / (1024 * 1024); //in mb
-    if (fileSize > MAX_FILEUPLOAD_SIZE) {
+    if(!["image/jpeg","image/png","image/svg+xml"].includes(file.type) ) {
+      event.target.setCustomValidity("Invalid image. Allowed image types are jpg, png and svg.");
+      event.target.reportValidity();
+    }
+    else if (fileSize > MAX_FILEUPLOAD_SIZE) {
       event.target.setCustomValidity("Max allowed size is " + MAX_FILEUPLOAD_SIZE + "MB.");
       event.target.reportValidity();
     } else {
@@ -182,7 +186,7 @@ class NewArtwork extends Component {
             <div className="form-group">
               <label htmlFor="exampleFormControlFile1">Upload Photo of Artwork</label>
               <input type="file" accept="image/*" className="form-control-file" onChange={this.handleUploadFile} required />
-              <span className="text-muted">Max upload size {MAX_FILEUPLOAD_SIZE}MB.</span>
+              <span className="text-muted">Allowed image types are jpg, png and svg.<br/>Max upload size {MAX_FILEUPLOAD_SIZE}MB.</span>
             </div>
           </div>
           {/* <div className="col-md-6 mb-3">
